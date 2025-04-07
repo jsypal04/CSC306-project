811058c9f67ed67852247b956321efc181a8eb9a
jsypal04 <60202320+jsypal04@users.noreply.github.com>
2025-04-07 11:15:10 -0400

    Wrote a solution to the dining philosophers problem with 2 philosophers. Also wrote a build script.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+src_ext=".c"
+
+for file in `ls`; do
+    rest=${file#*$src_ext}
+    index=$(( ${#file} - ${#rest} - ${#src_ext} ))
+
+    if [ $index -lt 0 ]; then
+        continue
+    fi 
+
+    name=${file:0:$index}
+
+    gcc -o $name $file
+done
--- dining_phils.c
+++ dining_phils.c
@@ -1,9 +1,45 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+typedef enum {
+    EATING,
+    THINKING
+} State;
+
+pthread_mutex_t chopstick_mutex;
+
+// Delays the current thread for `delay` seconds
+void delay_s(int delay) {
+
+    time_t start_time, current_time;
+    start_time = time(NULL);
+
+    do {
+        current_time = time(NULL);
+    } while (current_time - start_time < delay);
+}
+
 
 void* thread(void* arg) {
-    printf("Running thread %d.\n", (int)(long)arg);
+    int id = (int)(long)arg;
+    State state = THINKING;
+
+    while (1) {
+        if (state == EATING) {
+            pthread_mutex_lock(&chopstick_mutex);
+            printf("Thread %d is eating\n", id);
+            delay_s(id);
+            pthread_mutex_unlock(&chopstick_mutex);
+            state = THINKING;
+            printf("Thread %d is thinking\n", id);
+        }
+        else {
+            delay_s(1);
+            state = EATING;
+        }
+    }
 
     return NULL;
 }
