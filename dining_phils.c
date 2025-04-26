9d1a50e332a1999898c7fc38236d20d52f912f35
jsypal04 <60202320+jsypal04@users.noreply.github.com>
2025-04-26 11:20:30 -0400

    Wrote better comments for dining_phils.c

--- dining_phils.c
+++ dining_phils.c
@@ -3,17 +3,25 @@
 #include <stdio.h>
 #include <pthread.h>
 
+// Declarations
+
+// This type represents the state of a given philosopher
 typedef enum {
     THINKING,
     HUNGRY,
     EATING
 } state_t;
 
-uint8_t philosophers_eating = 0;
+uint8_t philosophers_eating = 0; // This variable is used to track the number of philosophers currently eating
+
+// Mutices to lock different resources/operations
+// There are a total of 7 mutices, 1 for each chopstick plus one for the pickup operation and one to update the counter
 pthread_mutex_t pickup_chopsticks_mutex;
 pthread_mutex_t counter_update;
 pthread_mutex_t chopstick_mutexes[5];
 
+
+// Function to delay a thread for a given number of seconds
 void delay_s(int delay) {
     time_t start_time, current_time;
     start_time = time(NULL);
@@ -23,47 +31,61 @@ void delay_s(int delay) {
     } while (current_time - start_time < delay);
 }
 
+// This is the function that will run for each concurrent philosopher thread
 void* philosopher_process(void* arg) {
-    int64_t id = (int64_t) arg;
-    state_t state;
-
-    uint8_t print_flag = 0;
+    int64_t id = (int64_t) arg; // get the thread id
+    state_t state = THINKING; // Inialize the state
 
     while (1) {
+        // If the philosopher is thinking, delay for a duration based on the thread id
+        // This ensures a variation in the time that different threads request different resources
         if (state == THINKING) {
-            // printf("Philosopher %ld is THINKING\n", id);
             delay_s(id + 1);
             state = HUNGRY;
         }
 
         // Aquire lock on pickup_chopsticks()
+        // This means only one philosopher can pickup chopsticks at a given time regardless of where they are seated
         pthread_mutex_lock(&pickup_chopsticks_mutex);
 
         // Only allow a thread to lock both adjacent chopsticks (not one)
+        // The pthread_mutex_trylock() function trys to lock the mutex but if it fails it does not block but it returns immediately
         if (pthread_mutex_trylock(&chopstick_mutexes[id]) != 0) {
+            // If locking fails, release the pickup chopsticks lock
             pthread_mutex_unlock(&pickup_chopsticks_mutex);
             continue;
         }
+        // Try to pick up the second chopstick
         if (pthread_mutex_trylock(&chopstick_mutexes[(id + 1) % 5]) != 0) {
+            // If locking fails, release lock on first chopstick and pickup chopsticks lock
             pthread_mutex_unlock(&chopstick_mutexes[id]);
             pthread_mutex_unlock(&pickup_chopsticks_mutex);
             continue;
         }
-        state = EATING;
-        pthread_mutex_lock(&counter_update);
-        philosophers_eating++;
-        pthread_mutex_unlock(&counter_update);
-        printf("Philosopher %ld is EATING\n", id);
-
+        
         // Release lock on pickup_chopsticks()
         pthread_mutex_unlock(&pickup_chopsticks_mutex);
 
+        // At this point in the loop, the current thread has locked both adjacent chopsticks
+        state = EATING;
+        
+        // increment the number of philosophers that are eating
+        pthread_mutex_lock(&counter_update);
+        philosophers_eating++;
+        pthread_mutex_unlock(&counter_update);
+        printf("Philosopher %ld is EATING\n", id); // print a status message
+
+
+        // the thread then delays (same as above) before returning to THINKING
         delay_s(id + 1);
         state = THINKING;
+
+        // Decrement the counter
         pthread_mutex_lock(&counter_update);
         philosophers_eating--;
         pthread_mutex_unlock(&counter_update);
 
+        // Release the locks on the chopsticks
         pthread_mutex_unlock(&chopstick_mutexes[id]);
         pthread_mutex_unlock(&chopstick_mutexes[(id + 1) % 5]);
     }
@@ -71,6 +93,8 @@ void* philosopher_process(void* arg) {
     return NULL;
 }
 
+// This function is used in a thread running concurrently with the philosophers
+// Its job is to print the current number of philosophers eating once per second
 void* print_counter(void* arg) {
 
     while (1) {
@@ -85,16 +109,19 @@ void* print_counter(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
+    // Declare variables
     pthread_t phil_1, phil_2, phil_3, phil_4, phil_5;
     pthread_t print_thread;
     int64_t id1, id2, id3, id4, id5;
 
+    // Initialize ids
     id1 = 0;
     id2 = 1;
     id3 = 2;
     id4 = 3;
     id5 = 4;
 
+    // create threads
     pthread_create(&phil_1, NULL, philosopher_process, (void*)id1);
     pthread_create(&phil_2, NULL, philosopher_process, (void*)id2);
     pthread_create(&phil_3, NULL, philosopher_process, (void*)id3);
@@ -102,6 +129,7 @@ int main(int argc, char* argv[]) {
     pthread_create(&phil_5, NULL, philosopher_process, (void*)id5);
     pthread_create(&print_thread, NULL, print_counter, NULL);
 
+    // wait for threads
     pthread_join(phil_1, NULL);
     pthread_join(phil_2, NULL);
     pthread_join(phil_3, NULL);
